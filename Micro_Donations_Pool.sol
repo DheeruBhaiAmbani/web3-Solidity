b35f4074898e99b96e614d0f308bb735b7bfd93e
Dheeraj Balani <dheerajbalani3030@gmail.com>
2025-02-21 15:23:36 +0530

    Update Micro_Donations_Pool.sol

--- Micro_Donations_Pool.sol
+++ Micro_Donations_Pool.sol
@@ -1,1 +1,26 @@
+pragma solidity ^0.8.0;
+
+contract DonationPool {
+    address public owner;
+    uint256 public totalDonations;
+    
+    mapping(address => uint256) public donations;
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not authorized");
+        _;
+    }
+    
+    function donate() public payable {
+        require(msg.value > 0, "Must send some ether");
+        donations[msg.sender] += msg.value;
+        totalDonations += msg.value;
+    }
+    
+    function withdraw() public onlyOwner {
+        uint256 amount = address(this).balance;
+        require(amount > 0, "No funds available");
+        payable(owner).transfer(amount);
+    }
+}
 
